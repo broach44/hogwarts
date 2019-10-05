0ce1be08ce5c28146f07715afa5d574fa084bfa7
Crystal Broach <crysland@gmail.com>
2019-10-05 12:13:19 -0500

    card styling complete

--- src/javascripts/components/studentList/studentList.js
+++ src/javascripts/components/studentList/studentList.js
@@ -1,6 +1,7 @@
 import utilities from '../../helpers/utilities';
 
 import studentData from '../../helpers/data/studentData';
+import houseData from '../../helpers/data/houseData';
 
 import studentCard from '../studentCard/studentCard';
 
@@ -9,9 +10,14 @@ import './studentList.scss';
 
 const createStudentList = () => {
   const students = studentData.getStudents();
+  const houses = houseData.getHouses();
   let domString = '<ul class="student-list">';
   for (let i = 0; i < students.length; i += 1) {
-    domString += studentCard.createStudentCard(students[i]);
+    for (let j = 0; j < houses.length; j += 1) {
+      if (students[i].houseId === houses[j].id) {
+        domString += studentCard.createStudentCard(students[i], houses[j]);
+      }
+    }
   }
   domString += '</ul>';
   utilities.printToDom('student-container', domString);
